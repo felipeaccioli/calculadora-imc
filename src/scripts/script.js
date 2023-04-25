e28bb713f25ba91b266782a13984d311dcb359f1
Felipe Accioli <felipeaccioli@gmail.com>
2023-04-25 15:42:14 -0300

    Atualizando o JavaScript

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,10 +1,46 @@
-const calcular = document.getElementById('calcular');
+// const calcular = document.getElementById('calcular');
 
-function imc () {
-  const nome = document.getElementById('nome');
-  const altura = document.getElementById('altura');
-  const peso = document.getElementById('peso');
-  const resultado = document.getElementById('resultado');
+// function imc () {
+//   const nome = document.getElementById('nome');
+//   const altura = document.getElementById('altura');
+//   const peso = document.getElementById('peso');
+//   const resultado = document.getElementById('resultado');
+// }
+
+// calcular.addEventListener('click', imc);
+
+function calcular(event) {
+  event.preventDefault();
+
+  let nomeInput = document.getElementById("nome").value;
+  
+  let alturaInput = document.getElementById("altura").value;
+
+  let pesoInput = document.getElementById ("peso").value;
+
+  let resultado = document.getElementById("resultado");
+
+  let calcular = (pesoInput / alturaInput^2);
+
+  if(calculo < 18.5) {
+    textResult.innerHTML = "Abaixo do peso.";
+    } else 
+      if (calculo < 25) {
+      textResult.innerHTML = "Peso normal.";
+      } else 
+      if (calculo < 30) {
+      textResult.innerHTML = "Acima do peso.";
+        } else
+        if (calculo < 35) {
+        textResult.innerHTML = "Obesidade grau I";
+          } else
+          if (calculo < 40) {
+          textResult.innerHTML = "Obesidade grau II";
+            } else
+            if (calculo > 40.1) {
+            textResult.innerHTML = "Obesidade grau III";
+            }
+  
+  contentResult.classList.remove("hide")
+
 }
-
-calcular.addEventListener('click', imc);
