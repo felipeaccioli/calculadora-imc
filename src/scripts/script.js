e4f6a494329a4152b7aea11b1f329ffa7a8c6cfa
Felipe Accioli <felipeaccioli@gmail.com>
2023-04-25 21:16:08 -0300

    Melhorando o JavaScript e implementando o resultado do cálculo do IMC na tela

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -7,28 +7,23 @@ function calcular(event) {
 
   let pesoInput = document.getElementById ("peso").value;
 
-  let resultadoResult = document.getElementById("resultado");
+  let resultado = document.getElementById("resultado");
 
   let textResult = document.getElementById("text-result");
 
-  let calcular = (pesoInput / (alturaInput^2));
+  let calculo = (pesoInput / (alturaInput * alturaInput));
 
   if(calculo < 18.5) {
     textResult.innerHTML = "Abaixo do peso.";
-    } else 
-      if (calculo < 25) {
+    } else if (calculo < 25) {
       textResult.innerHTML = "Peso normal.";
-      } else 
-      if (calculo < 30) {
-      textResult.innerHTML = "Acima do peso.";
-        } else
-        if (calculo < 35) {
-        textResult.innerHTML = "Obesidade grau I.";
-          } else
-          if (calculo < 40) {
-          textResult.innerHTML = "Obesidade grau II.";
-            } else
-            if (calculo > 40.1) {
-            textResult.innerHTML = "Obesidade grau III.";
+      } else if (calculo < 30) {
+        textResult.innerHTML = "Acima do peso.";
+        } else if (calculo < 35) {
+          textResult.innerHTML = "Obesidade grau I.";
+          } else if (calculo < 40) {
+            textResult.innerHTML = "Obesidade grau II.";
+            } else if (calculo > 40.1) {
+              textResult.innerHTML = "Obesidade grau III.";
             }
 }
